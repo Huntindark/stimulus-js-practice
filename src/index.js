0d68d12b7d0fa176d0c8541def74c2bac5db03fa
Federico <dartist@Federicos-MacBook-Air.local>
2022-01-30 17:24:02 -0300

    removed exemplary code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import { Application } from '@hotwired/stimulus'
-import ExampleController from './controllers/example_controller'
 import TodoController from './controllers/todo_controller'
 import RemoverController from './controllers/remover_controller'
 import ClipboardController from './controllers/clipboard_controller'
 
 application = Application.start()
 
-application.register('example', ExampleController)
 application.register('todo', TodoController)
 application.register('remover', RemoverController)
 application.register('clipboard', ClipboardController)
