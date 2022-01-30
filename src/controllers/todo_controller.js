95d074b0360edc96ee8c571e56bdff8e5b94bdec
Federico <dartist@Federicos-MacBook-Air.local>
2022-01-30 17:22:25 -0300

    more purging of extra code

--- src/controllers/todo_controller.js
+++ src/controllers/todo_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['input', 'list', 'delete']
+  static targets = ['input', 'list']
 
   add(event) {
     let li = `<li data-controller="remover clipboard">${this.inputTarget.value}<button type="button" data-action="click->remover#remove">Delete element</button> <button type="button" data-action="click->clipboard#copy">Copy line</button></li>`;
